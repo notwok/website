d27fb8ed9f9a24c4f66bd27d847868a5b7daf2fd
Jonathan <jonathanprice99@gmail.com>
2022-11-05 18:58:28 +0000

    Creating homepage for notwok, somehow

--- build/main.js
+++ build/main.js
@@ -1,10 +1,1 @@
-const title = document.querySelector('#hero-title');
-const titleText = Array(
-    'Leaner websites for a cleaner internet',
-    'Lean green website machine',
-    'Eco-friendly web design and hosting'
-)
-const random = Math.floor(Math.random() * titleText.length);
-
-window.onload = title.innerHTML = titleText[random];
-window.onload = title.classList.add('text-raisin');
+import 'tw-elements';
--- build/tailwind.config.cjs
+++ build/tailwind.config.cjs
@@ -1,26 +1,43 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./index.html",
-  "./src/**/*.{html,js}"],
+  "./src/**/*.{html,js}",
+  './node_modules/tw-elements/dist/js/**/*.js'],
   theme: {
     extend: {
-      colors: {
-        raisin: '#272727',
-        burlywood: '#d4aa7d',
-        champagne: '#efd09e',
-        springbud: '#d2d8b3',
-        pewter: '#90a9b7'
+      // colors: {
+      //   raisin: '#272727',
+      //   burlywood: '#d4aa7d',
+      //   champagne: '#efd09e',
+      //   springbud: '#d2d8b3',
+      //   pewter: '#90a9b7'
 
-      },
+      // },
       container: {
         padding: {
           DEFAULT: '1rem',
           sm: '2rem',
           lg: '2rem',
           xl: '3rem',
-      }
+      },
+      center: true,
     },
   },
-  plugins: [],
-}
+  
+},
+  plugins: [
+    require('@tailwindcss/typography'),
+    require("daisyui"),
+    require('tw-elements/dist/plugin')
+  ],
+  daisyui: {
+    styled: false,
+    themes: ["business"],
+    base: true,
+    utils: false,
+    logs: true,
+    rtl: false,
+    prefix: "",
+    darkTheme: "dark",
+  },
 }
